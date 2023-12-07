fa2767d12eb983419c77e0d922c41d4b93e61780
vishnu <vishnudeshmukh1222@gmail.com>
2023-12-07 23:32:40 +0530

    creating models

--- elevator_system/elevator/models.py
+++ elevator_system/elevator/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Elevator(models.Model):
+    operational = models.BooleanField(default=True)
+    in_maintenance = models.BooleanField(default=False)
+    current_floor = models.IntegerField(default=1)
+    moving_up = models.BooleanField(default=False)
+    requests = models.ManyToManyField('Request', related_name='elevators')
+
+class Request(models.Model):
+    floor = models.IntegerField()
+    direction = models.CharField(max_length=5, choices=[('up', 'Up'), ('down', 'Down')])
+    served = models.BooleanField(default=False)
