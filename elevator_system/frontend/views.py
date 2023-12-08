0e44584b9ad97e88d561b1ccf999f8a6d9fd218d
vishnu <vishnudeshmukh1222@gmail.com>
2023-12-08 22:19:50 +0530

    added ui

--- elevator_system/frontend/views.py
+++ elevator_system/frontend/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 # elevator_system/frontend/views.py
 from django.shortcuts import render
 
-def elevator_status(request):
+def elevator_status_man(request):
     return render(request, 'frontend/index.html')
